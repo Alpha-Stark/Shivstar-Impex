00a3e06c172b2920a832a642d150f881b80bd969
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-06-14 12:24:35 +0530

    styling button

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -38,9 +38,7 @@ function Navbar() {
             <div className="flex items-center">
                 <SignedOut>
                     <SignInButton>
-                        <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-black dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-                            Sign-In
-                        </button>
+                        <button className={`${styles.button} ${styles.dark}`}>Sign-In</button>
                     </SignInButton>
                 </SignedOut>
                 <SignedIn>

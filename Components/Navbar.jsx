0b7fc6d635754ae1cda60c0d7501aabd500ca966
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-06-14 12:16:56 +0530

    Headings changed

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -5,50 +5,49 @@ import styles from "../style/navbar.module.css";
 import logo from "../ShivstarLogo.png";
 import Image from "next/image";
 import Link from "next/link";
-import "../app/globals.css";
 
 function Navbar() {
     return (
-        <>
-            <div className={`${styles.navbaronly}`}>
-                <div className="flex items-center justify-between w-full md:w-auto mb-2 md:mb-0">
-                    <Link href="/" className="flex items-center p-1">
-                        <Image src={logo} alt="Logo" width={85} height={55} />
-                        <h4 className="text-white text-lg md:text-2xl ml-2 md:ml-4">SHIVSTAR IMPEX</h4>
-                    </Link>
-                </div>
-                <div className="flex flex-col md:flex-row md:ml-4 w-full md:w-auto justify-center md:justify-end">
-                    <Link href="/diamonds" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
-                        Diamonds
-                    </Link>
-                    <Link href="/watches" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
-                        Watches
-                    </Link>
-                    <Link href="/gift" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
-                        Gift
-                    </Link>
-                    <Link href="/contactUs" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
-                        Contact
-                    </Link>
-                    <Link href="/account" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
-                        Account
-                    </Link>
-                    <Link href="/bag" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
-                        Bag
-                    </Link>
-                </div>
-                <div className="flex items-center">
-                    <SignedOut>
-                        <SignInButton>
-                            <button className="bg-gray-900 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-full ring-1 ring-gray-500">Sign-In</button>
-                        </SignInButton>
-                    </SignedOut>
-                    <SignedIn>
-                        <UserButton />
-                    </SignedIn>
-                </div>
+        <div className={styles.navbaronly}>
+            <div className={styles["navbar-logo"]}>
+                <Link href="/" className="flex items-center p-1">
+                    <Image src={logo} alt="Logo" width={85} height={55} />
+                    <h4>SHIVSTAR IMPEX</h4>
+                </Link>
             </div>
-        </>
+            <div className="flex flex-col md:flex-row md:ml-4 w-full md:w-auto justify-center md:justify-end">
+                <Link href="/diamonds" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
+                    Diamonds
+                </Link>
+                <Link href="/watches" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
+                    Watches
+                </Link>
+                <Link href="/gift" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
+                    Gift
+                </Link>
+                <Link href="/contactUs" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
+                    Contact
+                </Link>
+                <Link href="/account" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
+                    Account
+                </Link>
+                <Link href="/bag" className={`${styles["navbar-link"]} my-1 md:my-0 mx-2 md:mx-4 p-1`}>
+                    Bag
+                </Link>
+            </div>
+            <div className="flex items-center">
+                <SignedOut>
+                    <SignInButton>
+                        <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-black dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                            Sign-In
+                        </button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </div>
     );
 }
 
